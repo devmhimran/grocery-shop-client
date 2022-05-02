fcf40af0e5ae03b48f6dfe60d3fd270c54e3faed
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-02 13:26:24 +0600

    Added Components

--- grocery-shop-client/src/App.js
+++ grocery-shop-client/src/App.js
@@ -1,15 +1,30 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import ErrorPage from './component/ErrorPage/ErrorPage';
 import HeaderNav from './component/HeaderNav/HeaderNav';
+import Home from './component/Home/Home';
+import ManageInventory from './component/ManageInventory/ManageInventory';
+import ManageItem from './component/ManageItems/ManageItems';
+import AddItem from './component/AddItem/AddItem';
+import MyItem from './component/MyItem/MyItem';
+import Blog from './component/Blog/Blog';
+import Login from './component/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <HeaderNav></HeaderNav>
        <Routes>
-        <Route path="/" element={} />
-        <Route path="about" element={} />
-      </Routes>
+         <Route path='/' element={<Home></Home>}></Route>
+         <Route path='/home' element={<Home></Home>}></Route>
+         <Route path='/manageinventory' element={<ManageInventory></ManageInventory>}></Route>
+         <Route path='/manageitems' element={<ManageItem></ManageItem>}></Route>
+         <Route path='/additem' element={<AddItem></AddItem>}></Route>
+         <Route path='/myitem' element={<MyItem></MyItem>}></Route>
+         <Route path='/blog' element={<Blog></Blog>}></Route>
+         <Route path='/login' element={<Login></Login>}></Route>
+         <Route path='*' element={<ErrorPage></ErrorPage>}></Route>
+      </Routes> 
     </div>
   );
 }
--- grocery-shop-client/src/component/HeaderNav/HeaderNav.js
+++ grocery-shop-client/src/component/HeaderNav/HeaderNav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import './HeaderNav.css';
 import groceryLogo from '../../assets/img/gorcery-logo.png';
@@ -6,17 +7,17 @@ const HeaderNav = () => {
     return (
         <Navbar className='navbar__main py-3' expand="lg">
             <Container>
-                <Navbar.Brand href="#home"><img className='brand__logo' src={groceryLogo} alt="" /></Navbar.Brand>
+                <Navbar.Brand as={Link} to="/"><img className='brand__logo' src={groceryLogo} alt="" /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Manage Inventory</Nav.Link>
-                        <Nav.Link href="#link">Manage Items</Nav.Link>
-                        <Nav.Link href="#link">Add Item</Nav.Link>
-                        <Nav.Link href="#link">My Item</Nav.Link>
-                        <Nav.Link href="#link">Blog</Nav.Link>
-                        <Nav.Link href="#link">Login</Nav.Link>
+                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/manageinventory">Manage Inventory</Nav.Link>
+                        <Nav.Link as={Link} to="/manageitems">Manage Items</Nav.Link>
+                        <Nav.Link as={Link} to="/additem">Add Item</Nav.Link>
+                        <Nav.Link as={Link} to="/myitem">My Item</Nav.Link>
+                        <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
