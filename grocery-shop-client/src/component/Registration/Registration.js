b82def7df1ba8e094a64b402d61ffe985a352cfe
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-05 18:31:44 +0600

    Update Registration

--- grocery-shop-client/src/component/Registration/Registration.js
+++ grocery-shop-client/src/component/Registration/Registration.js
@@ -7,33 +7,31 @@ import auth from '../firebase.init';
 
 const Registration = () => {
     const [errorMsg, setError] = useState('');
-    const [createUserWithEmailAndPassword,user,loading,error,] = useCreateUserWithEmailAndPassword(auth);
+    const [createUserWithEmailAndPassword, user, loading, error,] = useCreateUserWithEmailAndPassword(auth);
     const [sendEmailVerification, sending] = useSendEmailVerification(auth);
     const [updateProfile, updating, updateProfileError] = useUpdateProfile(auth);
     let loginError;
-
-    if(error){
-        loginError  = error.message;
+    console.log(user);
+    if (error) {
+        loginError = error.message;
     }
-    console.log(user);  
 
-
-    const handleRegistration = async(e) =>{
+    const handleRegistration = async (e) => {
         e.preventDefault();
         const name = e.target.name.value;
-        const phoneNumber = e.target.number.value;
+        // const phoneNumber = e.target.number.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
         const confirmPassword = e.target.confirmPassword.value;
-        // console.log(number);
-        if(password !== confirmPassword){
+        if (password !== confirmPassword) {
             const errorMsg = 'Password not matched';
             setError(errorMsg);
+        } else {
+            await createUserWithEmailAndPassword(email, password);
+            await updateProfile({ displayName: name });
+            await sendEmailVerification();
         }
-        await createUserWithEmailAndPassword( email, password);
-        await updateProfile({ displayName:name, phoneNumber});
-        await sendEmailVerification();
-        // console.log(name);
+
     }
     return (
         <div className='registration__main'>
@@ -44,23 +42,23 @@ const Registration = () => {
                         <form onSubmit={handleRegistration}>
                             <div className="input__form">
                                 <p className='py-2'>Enter Name</p>
-                                <input type="text" name="name" placeholder='Enter Your Name' required/>
+                                <input type="text" name="name" placeholder='Enter Your Name' required />
                             </div>
-                            <div className="input__form">
+                            {/* <div className="input__form">
                                 <p className='py-2'>Enter Number</p>
                                 <input type="text" name="number" placeholder='Enter Your Number' required/>
-                            </div>
+                            </div> */}
                             <div className="input__form">
                                 <p className='py-2'>Enter Email</p>
-                                <input type="email" name="email" placeholder='Enter Your Email' required/>
+                                <input type="email" name="email" placeholder='Enter Your Email' required />
                             </div>
                             <div className="input__form">
                                 <p className='py-2'>Enter Password</p>
-                                <input type="password" name="password" placeholder='Enter Your Password' required/>
+                                <input type="password" name="password" placeholder='Enter Your Password' required />
                             </div>
                             <div className="input__form">
                                 <p className='py-2'>Confirm Password</p>
-                                <input type="password" name="confirmPassword" placeholder='Enter Your Password' required/>
+                                <input type="password" name="confirmPassword" placeholder='Enter Your Password' required />
                                 <small className='text-danger'>{errorMsg}</small>
                                 <small className='text-danger'>{loginError}</small>
                             </div>
