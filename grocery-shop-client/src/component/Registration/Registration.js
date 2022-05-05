cc6919748fb94c798415f021dfb5595cd1f133d6
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-05 18:21:08 +0600

    Registration Complete

--- grocery-shop-client/src/component/Registration/Registration.js
+++ grocery-shop-client/src/component/Registration/Registration.js
@@ -2,35 +2,36 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import SocialLogin from '../SocialLogin/SocialLogin';
 import './Registration.css';
-import { useCreateUserWithEmailAndPassword, useSendEmailVerification } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSendEmailVerification, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../firebase.init';
 
 const Registration = () => {
     const [errorMsg, setError] = useState('');
-    const [
-        createUserWithEmailAndPassword,
-        user,
-        loading,
-        error,
-      ] = useCreateUserWithEmailAndPassword(auth);
-      console.log(user);
-      const [sendEmailVerification, sending] = useSendEmailVerification(
-        auth
-      );
-      
+    const [createUserWithEmailAndPassword,user,loading,error,] = useCreateUserWithEmailAndPassword(auth);
+    const [sendEmailVerification, sending] = useSendEmailVerification(auth);
+    const [updateProfile, updating, updateProfileError] = useUpdateProfile(auth);
+    let loginError;
+
+    if(error){
+        loginError  = error.message;
+    }
+    console.log(user);  
+
+
     const handleRegistration = async(e) =>{
         e.preventDefault();
         const name = e.target.name.value;
-        const number = e.target.number.value;
+        const phoneNumber = e.target.number.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
         const confirmPassword = e.target.confirmPassword.value;
-        
+        // console.log(number);
         if(password !== confirmPassword){
             const errorMsg = 'Password not matched';
             setError(errorMsg);
         }
         await createUserWithEmailAndPassword( email, password);
+        await updateProfile({ displayName:name, phoneNumber});
         await sendEmailVerification();
         // console.log(name);
     }
@@ -61,6 +62,7 @@ const Registration = () => {
                                 <p className='py-2'>Confirm Password</p>
                                 <input type="password" name="confirmPassword" placeholder='Enter Your Password' required/>
                                 <small className='text-danger'>{errorMsg}</small>
+                                <small className='text-danger'>{loginError}</small>
                             </div>
                             <button className='login__btn mt-3'>Sign up</button>
                         </form>
