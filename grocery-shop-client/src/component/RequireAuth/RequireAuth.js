8957eaff98a084309c72b97ca733e3accb9b3a35
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-06 19:59:44 +0600

    Added Email Verification

--- grocery-shop-client/src/component/RequireAuth/RequireAuth.js
+++ grocery-shop-client/src/component/RequireAuth/RequireAuth.js
@@ -3,25 +3,34 @@ import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/aut
 import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../firebase.init';
 import Loading from '../Loading/Loading';
+import './RequireAuth.css';
 
-const RequireAuth = ({children}) => {
+const RequireAuth = ({ children }) => {
     const [user, loading] = useAuthState(auth);
     const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
     const location = useLocation();
-    if(loading){
+    if (loading) {
         return <Loading></Loading>;
     }
-    if(!user){
-        return <Navigate to='/login' state={{from:location}} replace></Navigate>
+    if (!user) {
+        return <Navigate to='/login' state={{ from: location }} replace></Navigate>
     }
-    if(user.emailVerfied){
-        return <div className='container'>
-            <h3 className='text-danger'>Your email is not verified</h3>
-            <h2>Please verify your email</h2>
-            <button className='btn btn-outline-danger'>Send email again</button>
+    if (!user.emailVerified) {
+        return <div className='container text-center emailVerifySection'>
+            <div className="card w-50 mx-auto">
+                <div className="card-body py-5">
+                    <p>Your email is not verified</p>
+                    <p>Please verify your email</p>
+                    <button className='resendBtn' onClick={async () => {
+                        await sendEmailVerification();
+                        alert('Sent email');
+                    }}>Resend</button>
+                </div>
+            </div>
         </div>;
     }
     return children;
+    
 };
 
 export default RequireAuth;
