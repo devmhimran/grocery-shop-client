f0927a7452b65faa3ad5d9583fb6d86b810320ca
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-07 01:07:02 +0600

    Added Forgot Password & Toast

--- grocery-shop-client/src/component/ForgotPassword/ForgotPassword.js
+++ grocery-shop-client/src/component/ForgotPassword/ForgotPassword.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
 import './ForgotPassword.css';
+import auth from '../firebase.init';
+import toast, { Toaster } from 'react-hot-toast';
 
 const ForgotPassword = () => {
+    const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(auth);
+    const handleReset = async (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        await sendPasswordResetEmail(email);
+        toast('Please Check Your Email', {
+            icon: '📧',
+          });
+          e.target.reset();
+    }
     return (
         <div className='forgetPass__main'>
             <div className="container">
                 <div className="card forgetPass__form__card">
                     <div className="card-body">
                         <h2>Reset Your Password?</h2>
-                        <form>
+                        <form onSubmit={handleReset}>
                             <div className="input__form">
                                 <p className='py-2'>Enter Email</p>
                                 <input type="email" name="email" placeholder='Enter Your Email' />
@@ -18,6 +31,8 @@ const ForgotPassword = () => {
                         </form>
                     </div>
                 </div>
+                <Toaster position="top-center"
+                    reverseOrder={false} />
             </div>
         </div>
     );
