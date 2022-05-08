40fada19532b34c3c58fc73a0701fbea66259a99
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-08 23:01:18 +0600

    Inventory Deliver & Update Added

--- grocery-shop-client/src/component/Inventory/Inventory.js
+++ grocery-shop-client/src/component/Inventory/Inventory.js
@@ -1,3 +1,4 @@
+import { async } from '@firebase/util';
 import React, { useEffect, useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 import { useLocation, useParams } from 'react-router-dom';
@@ -28,33 +29,68 @@ const Inventory = () => {
             alert('Please Enter Some Quantity');
             e.target.reset()
         } else {
-            const dataQuantity = quantity;
-            const totalQuantity = parseInt(inputQuantity) + parseInt(dataQuantity);
-            const updateQuantity = { quantity: totalQuantity }
-            console.log(updateQuantity, typeof (updateQuantity))
-            fetch(`http://localhost:5000/update/${id}`, {
-                method: "PUT",
-                headers: {
-                    "content-type": "application/json"
-                },
-                body: JSON.stringify(updateQuantity)
-            })
-                .then(res => res.json())
-                .then(data => {
-                   const isProceed = window.confirm('Are You Sure Update Data?');
-                   
-                   if(isProceed){
-                    window.location.reload();
-                   }
-                   alert('Added')
-                   toast.success('Successfully Updated!')
-                    console.log(data);
+            const isProceed = window.confirm('Are You Sure Update Data?');
+            if(isProceed){
+                const dataQuantity = quantity;
+                const totalQuantity = parseInt(inputQuantity) + parseInt(dataQuantity);
+                const updateQuantity = { quantity: totalQuantity }
+                console.log(updateQuantity, typeof (updateQuantity))
+                fetch(`http://localhost:5000/update/${id}`, {
+                    method: "PUT",
+                    headers: {
+                        "content-type": "application/json"
+                    },
+                    body: JSON.stringify(updateQuantity)
                 })
-            e.target.reset();
-            // location.reload(); 
+                    .then(res => res.json())
+                    .then(data => {
+                       
+                       
+                       if(isProceed){
+                        
+                        window.location.reload();
+                       }
+                    
+                      
+                        console.log(data);
+                    })
+                e.target.reset();
+                // location.reload(); 
+                toast.success('Successfully Updated!')
+            }
             
         }
 
+    }
+    let quantityNow =  parseInt(quantity);
+    const handleDeliver = (e) =>{
+        // const quantityNow = quantity;
+      e.preventDefault();
+        if(quantityNow === 0){
+            alert('Product Is out of Stock')
+        }else{
+            const isProceed = window.confirm('Are You Wanted To Deliver?');
+            if(isProceed){
+                quantityNow = quantityNow - 1;
+                const newQuantity = { quantity: quantityNow }
+              fetch(`http://localhost:5000/update/${id}`, {
+                    method: "PUT",
+                    headers: {
+                        "content-type": "application/json"
+                    },
+                    body: JSON.stringify(newQuantity)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                       console.log(data)
+                        window.location.reload();     
+                    })
+                    toast.success('Successfully Delivered!') 
+            }
+            
+        }    
+        
+        
     }
 
     return (
@@ -62,12 +98,13 @@ const Inventory = () => {
             <div className="card single__product__card">
                 <div className="card-body single__product__card__body">
                     <div className="row">
-                        <div className="col-lg-6 col-md-6 col-sm-12">
+                        <div className="col-lg-6 col-md-12 col-sm-12">
                             <div className="single__product__image__container">
                                 <img className='single__product__image' src={image} alt="" />
                             </div>
                         </div>
-                        <div className="col-lg-6 col-md-6 col-sm-12 d-flex align-items-center">
+                        <div className="col-lg-6 col-md-12 col-sm-12 d-flex align-items-center">
+                            <div className="product__detail__main">
                             <div className="product__detail">
                                 <small>Id: {_id}</small>
                                 <h1 className='single__product__name'>{name}</h1>
@@ -75,15 +112,16 @@ const Inventory = () => {
                                 <p className='single__product__quantity'>Quantity: {quantity} {unit}</p>
                                 <p className='single__product__supplierName'>{supplierName}</p>
                                 <div className="inventory__update__button d-inline">
-                                    <div className="product__update__main">
-                                        <form onSubmit={handleQuantityUpdate}>
+                                    <div className="product__update__main d-inline">
+                                        <form onSubmit={handleQuantityUpdate}  className="d-inline">
                                             <input className='grocery__quantity' type="number" name="quantity" min="0" onKeyPress={preventMinus} />
                                             <button className='grocery__quantity__button mx-2'>Update</button>
                                         </form>
-                                        <button className='grocery__quantity__button'>Delivered</button>
+                                        <button className='grocery__quantity__button' onClick={handleDeliver}>Delivered</button>
                                     </div>
                                 </div>
 
+                            </div>
                             </div>
                         </div>
                     </div>
