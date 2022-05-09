03bb2c9e9b16dd1cbbeaa17c70858638858f57c6
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-09 20:11:47 +0600

    Update Manage Inventory

--- grocery-shop-client/src/component/ManageInventory/ManageInventory.js
+++ grocery-shop-client/src/component/ManageInventory/ManageInventory.js
@@ -1,16 +1,61 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import useProducts from '../Hooks/useProducts';
 import Products from '../Products/Products';
 
 const ManageInventory = () => {
     const [products, setProducts] = useProducts();
+    const navigate = useNavigate();
+    const handleProductDetail = (id) => {
+        navigate(`/inventory/${id}`);
+    }
+    const handleDelete = (id) => {
+        const proceed = window.confirm('Are your sure?');
+        if (proceed) {
+    
+            const url = `https://gentle-plateau-17754.herokuapp.com/inventory/${id}`;
+            fetch(url, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    const remainingService = products.filter(products => products._id !== id);
+                    setProducts(remainingService);
+                    console.log(data);
+                })
+        }
+    }
     return (
         <div className="manage__Inventory">
                 <h1 className='text-center'>Manage Inventory</h1>
                 <div className="container">
                     <div className="row mt-5 g-3">
                         {
-                            products.map(productData => <Products key={productData._id} products={productData}></Products>)
+                            products.map(productData => 
+                                <div className="col-lg-4 col-md-6 col-sm-12">
+                                <div className='card product__card'>
+                                    <div className="card-body mx-auto product__card__body mx-auto">
+                                        <div className="product__image__container">
+                                            <img className='product__image' src={productData.image} alt="" />
+                                        </div>
+                                        <div className="product__detail mt-4">
+                                            <h4 className='product__name'>{productData.name}</h4>
+                                            <div className="product__sub__description d-flex justify-content-between">
+                                                <p className='product__price'>BDT {productData.price} /-</p>
+                                                <p className='product__quantity'>{productData.quantity} {productData.unit}</p>
+                                            </div>
+                                            <p className='product__supplier my-2'>Supplier: {productData.supplierName}</p>
+                                            <p className='product__description'>{productData.shortDescription.slice(0, 85)}</p>
+                                        </div>
+                                        <div className="product__button d-flex justify-content-between">
+                                            <button className='product__btn mt-4' onClick={() => handleProductDetail(productData._id)}>Update</button>
+                                            <button className='product__btn mt-4' onClick={() => handleDelete(productData._id)}>Delete</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            )
                         }
 
                     </div>
--- grocery-shop-client/src/component/Products/Products.js
+++ grocery-shop-client/src/component/Products/Products.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import useProducts from '../Hooks/useProducts';
 import './Product.css';
 
 const Products = (props) => {
     const { _id, name, image, shortDescription, price, quantity, unit, supplierName } = props.products;
     const navigate = useNavigate();
-    const handleProductDetail =(id) =>{
+    const [products, setProducts] = useProducts([]);
+    const handleProductDetail = (id) => {
         navigate(`/inventory/${id}`);
-
     }
+    console.log(products)
+    
     return (
         <div className="col-lg-4 col-md-6 col-sm-12">
             <div className='card product__card'>
@@ -20,12 +23,15 @@ const Products = (props) => {
                         <h4 className='product__name'>{name}</h4>
                         <div className="product__sub__description d-flex justify-content-between">
                             <p className='product__price'>BDT {price} /-</p>
-                            <p className='product__quantity'>{quantity} {unit}</p> 
+                            <p className='product__quantity'>{quantity} {unit}</p>
                         </div>
                         <p className='product__supplier my-2'>Supplier: {supplierName}</p>
                         <p className='product__description'>{shortDescription.slice(0, 85)}</p>
                     </div>
-                    <button className='product__btn mt-4' onClick={()=>handleProductDetail(_id)}>Update</button>
+                    <div className="product__button d-flex justify-content-between">
+                        <button className='product__btn mt-4' onClick={() => handleProductDetail(_id)}>Update</button>
+                        <button className='product__btn mt-4' onClick={() => props.handleDelete(_id)}>Delete</button>
+                    </div>
                 </div>
             </div>
         </div>
